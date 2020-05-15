90a6e4706845d592d061fa6268b0cd289d7ebe40
bikrant1999 <wbikrant1999@gmail.com>
2020-05-16 02:02:03 +0530

    updated

--- hotel.js
+++ hotel.js
@@ -1,7 +1,15 @@
 $(document).ready(()=>{
     $('.f-i').click(function(){
+        const pastid = $('.facilities-icons div.active').attr('for');
+        const fade = 200;
+        $(`#${pastid}`).fadeOut({duration:fade});
         $('.facilities-icons div.active').removeClass('active');
-        $(this).addClass('active')
+        $(this).addClass('active');
+        const id = $(this).attr('for');
+        setTimeout(()=>{
+            $(`#${id}`).fadeIn({duration:fade});
+        },fade)
+        
     })
 	
 }) 
